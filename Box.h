9e00dc557728ef52121ddd80501d4ecf143940c0
e11438 <isankalpa7@gmail.com>
2020-12-16 08:47:57 +0530

    Create Box.h

---
+++ Box.h
@@ -0,0 +1,9 @@
+class Box {
+
+   public:
+      double length;   // Length of a box
+      double breadth;  // Breadth of a box
+      double height;   // Height of a box
+    Box(){};
+    ~Box(){};
+};
